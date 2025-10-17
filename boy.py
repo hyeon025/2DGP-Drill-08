4f6e3b5b0703a8afcbff5a15a30217fe86a85e57
hyeon025 <kjh04255@naver.com>
2025-10-17 18:37:05 +0900

    autorun y값 20 증가시킴

--- boy.py
+++ boy.py
@@ -53,9 +53,9 @@ class AutoRun:
 
     def draw(self):
         if self.boy.face_dir == 1:  # right
-            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y,200,200)
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y + 20,200,200)
         else:  # face_dir == -1: # left
-            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y,200,200)
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y + 20,200,200)
 
 
 class Run:
