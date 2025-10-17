53aa52176d3ed39a0a197e4a7ae2759a26f86ce2
hyeon025 <kjh04255@naver.com>
2025-10-17 19:21:14 +0900

    Drill #8 제출 - 2024182009 김정현

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ running = True
 
 open_canvas()
 reset_world()
-# game loop
+
 while running:
     handle_events()
     update_world()
     render_world()
     delay(0.01)
-# finalization code
+
 close_canvas()
